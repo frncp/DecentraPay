51741d9cce1e219d1dc801a7110767d048e98efa
User1 <stefanoverrilli00@gmail.com>
2021-08-27 15:55:24 +0200

    Add Contracts for payment

---
+++ SolidityFolder/ContractA.sol
@@ -0,0 +1,82 @@
+pragma solidity ^0.8.4;
+//Questo contratto deve ricevere i pagamenti in cui calcolare lo sconto e quelli in cui si vuole consumare uno sconto
+
+
+interface ContractB{
+    function payAndRequestDiscount(address _address,uint256 DiscountRequest) external payable;
+    function useDiscountAndDelete(address payable _address,uint256 DiscountRequest) external payable;
+}
+
+contract contractA{
+    
+    event Paysent(uint256 value);
+    
+    address private Storage_address;
+    
+    uint256 actualDiscount;
+    
+    address owner = address(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4);
+    
+    constructor(address _StorageAddress){
+        Storage_address=_StorageAddress;
+    }
+    
+    
+    function payRequireDiscount(address payable _address)  amountAboveZero(msg.value) validAddress(_address) external payable{
+        ContractB StorageContract = ContractB(Storage_address);
+        uint256 calcDiscount = uint(msg.value*actualDiscount/100);
+        require((calcDiscount/10000)*10000 == calcDiscount);
+        StorageContract.payAndRequestDiscount{value: calcDiscount}(_address,calcDiscount);
+        emit Paysent(calcDiscount);
+    }
+    
+    function payAndApplyDiscount(address payable _address,uint RequestedDiscount) amountAboveZero(msg.value) validAddress(_address) external payable{
+        ContractB StorageContract = ContractB(Storage_address);
+        StorageContract.useDiscountAndDelete(_address,RequestedDiscount);
+    }
+    
+    modifier OnlyOwnerof(){
+        require(msg.sender == owner);
+        _;
+    }
+    
+    modifier amountAboveZero(uint _amount){
+        require(_amount != 0);
+        _;
+    }
+    
+    modifier validAddress(address _address){
+        require(_address != address(0));
+        _;
+    }
+    
+    function setStorageContract(address _address) external OnlyOwnerof{
+        Storage_address = _address;
+    }
+    
+    function getStorageContract() external OnlyOwnerof view returns(address){
+        return Storage_address;
+    }
+    
+    function SetDiscount(uint _discount) external OnlyOwnerof{
+        actualDiscount = _discount;
+    }
+    
+    function getCurrentDiscount() external view returns(uint){
+        return actualDiscount;
+    }
+    
+    function getBalance() external OnlyOwnerof view returns(uint){
+        return address(this).balance;
+    }
+    
+    function setOwner(address payable _address) external OnlyOwnerof{
+        owner = _address;
+    }
+    
+    function withdrow() external OnlyOwnerof{
+    payable(msg.sender).transfer(address(this).balance);
+
+    }
+    
+}
---
+++ SolidityFolder/ContractB.sol
@@ -0,0 +1,62 @@
+pragma solidity ^0.8.4;
+
+
+
+contract ContractB{
+    
+mapping(address => uint256) AddrMap;    
+address[] private Accounts;
+
+address private bAddress;
+
+event PaymentDone(address sender,uint value);
+
+
+function useDiscountAndDelete(address payable _address,uint DiscountRequest) external payable{
+    require(DiscountRequest <= AddrMap[_address],"Balance not sufficent");
+    require((DiscountRequest/10000)*10000 == DiscountRequest);
+    AddrMap[_address] -= DiscountRequest;
+    _address.transfer(DiscountRequest);
+    emit PaymentDone(msg.sender,msg.value);
+
+}
+
+
+function payAndRequestDiscount(address _address,uint256 DiscountRequest) external payable{
+    addAddress(_address);
+    addDiscount(_address,DiscountRequest);
+}
+
+function addDiscount(address _address,uint _discountRequest) internal{
+    AddrMap[_address] += _discountRequest;
+}
+
+function GetDiscount(address _address) public view returns(uint){
+    return AddrMap[_address];
+}
+
+function CheckIfAddressExists(address _address) private view returns (bool){
+    for (uint i = 0; i<Accounts.length;i++){
+            if(Accounts[i]== _address)
+                return true;}
+    return false;
+}
+
+function addAddress(address _address) internal{
+    if(!CheckIfAddressExists(_address)){
+        Accounts.push(_address);
+    }
+}
+
+function ContractBalance() public view returns(uint256){
+    return address(this).balance;
+}
+
+function getBalanceInEther() external view returns(uint){
+        return ((address(this).balance) ether);
+    }
+
+function getAccounts() external view returns(address[] memory){
+    return Accounts;
+}
+}
