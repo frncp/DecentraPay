75108477cdd476d1d521a17905513c79175b2a85
StefanoVerrilli <stefanoverrilli00@gmail.com>
2021-08-28 16:35:44 +0200

    fetch of fields data

--- DappFolder/Functions.js
+++ DappFolder/Functions.js
@@ -8,13 +8,13 @@ function enabledisabletextbox(){
 }
 
 function submitRequest(){
-  var payament = document.getElementById("spendnum").value;
+  var payment = document.getElementById("spendnum").value;
   var textbox = document.getElementById("DiscountAmount");
   var checkbox = document.getElementById("Selected");
   if(checkbox.checked){
       var discountRequested = textbox.value;
   }
-  console.log("Pagamento Effettuato: " + payament);
+  console.log("Pagamento Effettuato: " + payment);
   console.log("Sconto Richiesto: " + discountRequested);
   //Inizializza chiamata allo smart contract con payment e discountRequested;
 }
